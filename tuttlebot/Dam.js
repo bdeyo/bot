17807a8e05ede0683e3310f5420452f2c56eba17
Ben DeYoung <deyo0039@gmail.com>
2016-05-04 18:33:23 -0500

    Create Dam.js

---
+++ tuttlebot/Dam.js
@@ -0,0 +1,10 @@
+var env = require('../config.json')
+
+var DamModule = function () {};
+
+WhyModule.prototype.Message = function(keyword, message, callback) {
+	var pic = 'https://i.imgur.com/7lZwLKc.jpg';
+	return callback(pic);
+}
+
+module.exports = DamModule;
