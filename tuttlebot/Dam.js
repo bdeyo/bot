8eefc66b0c67b373fe74e7be402ba6b0d53561fa
Ben DeYoung <deyo0039@gmail.com>
2016-05-04 18:37:34 -0500

    Update Dam.js

--- tuttlebot/Dam.js
+++ tuttlebot/Dam.js
@@ -2,7 +2,7 @@ var env = require('../config.json')
 
 var DamModule = function () {};
 
-WhyModule.prototype.Message = function(keyword, message, callback) {
+DamModule.prototype.Message = function(keyword, message, callback) {
 	var pic = 'https://i.imgur.com/7lZwLKc.jpg';
 	return callback(pic);
 }
