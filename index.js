21fad7ea1344daad2322ce6a4b567d54e4e2763c
Ben DeYoung <deyo0039@gmail.com>
2016-08-03 19:47:03 -0500

    added rolloff

--- index.js
+++ index.js
@@ -111,6 +111,25 @@ var commands = {
 			}
 		}
 	},
+	"rolloff": {
+		usage: "",
+		description: "Roll off against the bot (D20)",
+		process: function(bot, msg) {
+			var roll_bot;
+			var roll_usr;
+			
+			bot.sendMessage(msg.channel, "A CHALLENGER APPROACHES!")
+			roll_bot = d20.roll(20);
+			roll_usr = d20.roll(20);
+			
+			if (roll_bot > roll_usr)
+				bot.sendMessage(msg.channel, "MUAHAHAHA. My " + roll_bot + " p4wned your " + roll_usr + ". Get rekt.")
+			else if (roll_usr > roll_bot)
+				bot.sendMessage(msg.channel, "Lucky roll... My " + roll_bot + " can't beat your " + roll_usr + ".")
+			else
+				bot.sendMessage(msg.channel, "Tie? TIE!? Let's call that a win for me...")
+		}
+	},
 	"blown": {
 		usage: "",
 		description: "returns gif",
