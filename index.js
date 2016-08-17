f4bec7c887763355676560200d6105b76b591a25
Ben DeYoung <deyo0039@gmail.com>
2016-08-16 20:00:17 -0500

    role check works
    now trying to read values from json...

--- index.js
+++ index.js
@@ -179,7 +179,9 @@ var commands = {
 		process: function(bot, msg) {
 			var roles = msg.channel.server.roles;
 			if (msg.author.hasRole(roles[1]))
-				bot.sendMessage(msg.channel, "Good afternoon, Mr. Admin");
+				bot.sendMessage(msg.channel, "Good afternoon, Mr. Admin.  JSON test below");
+				var tmp = JSON.parse(Stats);
+ +				bot.sendMessage(msg.channel, tmp);
 			else
 				bot.sendMessage(msg.channel, "Frig off...")
 		}
