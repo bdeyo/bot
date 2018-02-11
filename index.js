9b9c282c0a57d19ea004009320d1818fb9400930
Ben <deyo0039@gmail.com>
2018-02-11 02:14:19 -0600

    updated bot

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
 try {
-	var Config = require('./config.json');
-} catch (e){
-	Config.debug = false;
-	Config.respondToInvalid = false;
-}
-
-try {
 	var AuthDetails = require('./auth.json');
 } catch (e){
 	console.log("Please create an auth.json file with account email and password"+e.stack);
@@ -13,7 +6,7 @@ try {
 }
 
 try {
-	var Discord = require("discord.js");
+	var Discord = require('discord.io');
 } catch (e){
 	console.log("Please run npm install discord.js"+e.stack);
 	process.exit();
@@ -32,61 +25,67 @@ try {
 	Stats.debug = true;
 }
 
-var bot = new Discord.Client();
+
+var bot = new Discord.Client({
+	token: AuthDetails.token,
+	autorun: true
+});
+console.log("username: " + bot.username + " ID: " + bot.id);
 
 var commands = {
 	"dam": {
 		usage: "",
 		description: "returns image/gif",
-		process: function(bot, msg) {
+		process: function(bot, channel) {
 			var rand = Math.floor(Math.random() * 2) + 1;
 			var result;
 			if (rand == 1)
 				result = 'https://i.imgur.com/7lZwLKc.jpg';
 			else
 				result = 'https://i.imgur.com/VQLGJOL.gif';
-			bot.sendMessage(msg.channel, result);
+			bot.sendMessage({to: channel, message: result});
 		}
 	},
 	"happy": {
 		usage: "",
 		description: "returns image/gif",
-		process: function(bot, msg) {
+		process: function(bot, channel) {
 			var rand = Math.floor(Math.random() * 2) + 1;
 			var result;
 			if (rand == 1)
 			  result = 'https://i.imgur.com/gbXzYXp.gif';
 			else
 			  result = 'https://i.imgur.com/OxwIC0O.gif';
-			bot.sendMessage(msg.channel, result);
+			bot.sendMessage({to: channel, message: result});
 		}
 	},
 	"horse": {
 		usage: "",
 		description: "returns horse gif",
-		process: function(bot, msg) {
+		process: function(bot, channel) {
 			var result = 'https://i.giphy.com/S4mOmgmpBQAXm.gif';
-			bot.sendMessage(msg.channel, result);
+			bot.sendMessage({to: channel, message: result});
 		}
 	},
-	"img": {
+	//NEEDS UPDATE
+	"zzzimg": {
 		usage: "<search term>",
 		description: "searches imgur based on keyword",
-		process: function(bot, msg, suffix) {
+		process: function(bot, channel, suffix) {
 			var key = AuthDetails;
 			var Imgur = require('imgur-search');
 			
 			var term = suffix;
 			if(!term) {
-				bot.sendMessage(msg.channel, "No Search Term");
+				bot.sendMessage({to: channel, message:"No Search Term"});
 				return;
 			}
 			new Imgur(key.imgur_key).search(term).then(function(result) {
 				if (result.length == 0 || result == undefined) {
-					bot.sendMessage(msg.channel, "You suck at searching");
+					bot.sendMessage({to: channel, message:"You suck at searching"});
 				} else {
 					var img = result[Math.floor(Math.random() * result.length)];
-					bot.sendMessage(msg.channel, img.link);
+					bot.sendMessage({to: channel, message: img.link});
 				}
 			});
 		}
@@ -94,55 +93,55 @@ var commands = {
 	"why": {
 		usage: "",
 		description: "returns gif",
-		process: function(bot, msg) {
+		process: function(bot, channel) {
 			var result = 'https://media.giphy.com/media/1M9fmo1WAFVK0/giphy.gif';
-			bot.sendMessage(msg.channel, result);
+			bot.sendMessage({to: channel, message: result});
 		}
 	},
 	"roll": {
 		usage: "[#d#]",
 		description: "dice roller, with single command giving a d20, otherwise rolling specified combo",
-		process: function(bot, msg, suffix) {
+		process: function(bot, channel, suffix) {
 			if(!suffix) {
-				bot.sendMessage(msg.channel, msg.author + " rolled: " + d20.roll(20));
+				bot.sendMessage({to: channel, message:"rolled: " + d20.roll(20)});
 			} else {
 				var result;
 				try {
 					result = d20.roll(suffix);
 				} catch (e){
-					bot.sendMessage(msg.channel, msg.author + " Could not roll " + suffix);
+					bot.sendMessage({to: channel, message:"Could not roll " + suffix});
 					return;
 				}
 				if (!isNaN(result) && result != 'NaN')
-					bot.sendMessage(msg.channel, msg.author + " rolled: " + d20.verboseRoll(suffix));
+					bot.sendMessage({to: channel, message:"Rolled: " + d20.verboseRoll(suffix)});
 				else
-					bot.sendMessage(msg.channel, msg.author + " Not a valid number");
+					bot.sendMessage({to: channel, message:"Not a valid number"});
 			}
 		}
 	},
 	"rolloff": {
 		usage: "",
 		description: "Roll off against the bot (D20)",
-		process: function(bot, msg) {
+		process: function(bot, channel) {
 			var roll_bot;
 			var roll_usr;
 			
 			roll_bot = d20.roll(20);
 			roll_usr = d20.roll(20);
-			bot.sendMessage(msg.channel, "A CHALLENGER APPROACHES!");
+			bot.sendMessage({to: channel, message:"A Challenger APPROACHES!"});
 			
 			if (roll_bot > roll_usr)
-				bot.sendMessage(msg.channel, "MUAHAHAHA. My " + roll_bot + " p4wned your " + roll_usr + ". Get rekt.");
+				bot.sendMessage({to: channel, message:"MUAHAHAHA. My " + roll_bot + " p4wned your " + roll_usr + ". Get rekt."});
 			else if (roll_usr > roll_bot)
-				bot.sendMessage(msg.channel, "Lucky roll... My " + roll_bot + " can't beat your " + roll_usr + ".");
+				bot.sendMessage({to: channel, message:"Lucky roll... My " + roll_bot + " can't beat your " + roll_usr + "."});
 			else
-				bot.sendMessage(msg.channel, "Tie? TIE!? Let's call that a win for me...");
+				bot.sendMessage({to: channel, message:"Tie? TIE!? Let's call that a win for me..."});
 		}
 	},
 	"blown": {
 		usage: "",
 		description: "returns gif",
-		process: function(bot, msg) {
+		process: function(bot, channel) {
 			var result;
 			var rand = Math.floor(Math.random() * 6);
 			var gifs = [	'https://i.imgur.com/YGsTG5o.gif',
@@ -153,13 +152,13 @@ var commands = {
 					'https://i.imgur.com/3WB3L.gif',
 					'https://giphy.com/gifs/cheezburger-robots-screaming-NfEQd1ge1kP04']
 					
-			bot.sendMessage(msg.channel, gifs[rand]);
+			bot.sendMessage({to: channel, message:gifs[rand]});
 		}
 	},
 	"weekend": {
 		usage: "",
 		description: "returns days until weekend",
-		process: function(bot, msg) {
+		process: function(bot, channel) {
 			var date = new Date();
 			var day = date.getDay();
 			var result = [  "Last day, Enjoy it!", 
@@ -170,83 +169,59 @@ var commands = {
 					"FRIDAY!",
 					"Less than 48 hours left..."];
 					
-			bot.sendMessage(msg.channel, result[day]);
+			bot.sendMessage({to: channel, message:result[day]});
 		}
 	},
 	"test": {
 		usage: "Admin only",
 		description: "",
-		process: function(bot, msg, suffix) {
-			var roles = msg.channel.server.roles;
+		process: function(bot, channel, suffix) {
+			var roles = channel.server.roles;
 			if (suffix == "read"){
 				if (msg.author.hasRole(roles[1])) {
-					bot.sendMessage(msg.channel, "Good afternoon, Mr. Admin.  JSON test below");
-					bot.sendMessage(msg.channel, Stats.members);
+					bot.sendMessage({to: channel, message:"Good afternoon, Mr. Admin.  JSON test below"});
+					bot.sendMessage({to: channel, message:Stats.members});
 				} else
-					bot.sendMessage(msg.channel, "Frig off...");
+					bot.sendMessage({to: channel, message:"Frig off..."});
 			}
 			if (suffix.match(/^write/)) {
-				if (msg.author.hasRole(roles[1]))
-					bot.sendMessage(msg.channel, "Still adding write");
+				if (channel.author.hasRole(roles[1]))
+					bot.sendMessage({to: channel, message:"Still adding write"});
 				else
-					bot.sendMessage(msg.channel, "Don't even try...");
+					bot.sendMessage({to: channel, message:"Don't even try..."});
 			}
 		}
 	}
 };
 
-bot.on("ready", function () {
+bot.on('ready', function () {
 	console.log("Ready to start. Channels: " + bot.channels.length);
 });
+
 
 bot.on('disconnected', function() {
 	console.log('Disconnected.');
 	process.exit(1);
 });
 
-bot.on("message", function (msg) {
-	if(msg.author.id != bot.user.id && msg.content[0] === '!'){
-		var cmdTxt = msg.content.split(" ")[0].substring(1);
-        	var suffix = msg.content.substring(cmdTxt.length+2);
-		var cmd = commands[cmdTxt];
+bot.on('message', function (user, userID, channelID, message, evt) {
+	if (message.substring(0,1) == '!'){
+		var args = message.substring(1).split(' ');
+		var cmd = args[0];
+		var suffix = args[1];
+		var result = commands[cmd];
 		
-        	if(cmdTxt === "help"){
-			var info = "Available Commands: ";
-			for(var cmd in commands) {
-				info += "!" + cmd;
-				var usage = commands[cmd].usage;
-				if(usage){
-					info += " " + usage + "; ";
-				}
-				else
-					info += "; ";
-			}
-			bot.sendMessage(msg.author,info);
-        	} else if(cmd) {
-			try{
-				cmd.process(bot,msg,suffix);
-			} catch(e){
-				if(Config.debug){
-					bot.sendMessage(msg.channel, "command " + cmdTxt + " failed :(\n" + e.stack);
-				}
+		if (cmd == 'help'){
+			var info = "Help is being updated!";
+			bot.sendMessage({to: userID, message: info});
+		} else if (result){
+			try {
+				result.process(bot,channelID,suffix);
+			} catch (e){
+				console.log("Command failed: " + cmd + ". " + e.stack);
 			}
 		} else {
-			bot.sendMessage(msg.author, " Invalid command, Sucka!  Maybe you need some !help");
+			bot.sendMessage({to: channelID, message: "Not a command, use !help"});
 		}
-	} else if (msg.author != bot.user && msg.isMentioned(bot.user)) {
-		bot.sendMessage(msg.channel,msg.author + ", you called?");
-    	}
+	}
 });
-
-exports.addCommand = function(commandName, commandObject){
-    try {
-        commands[commandName] = commandObject;
-    } catch(err){
-        console.log(err);
-    }
-}
-exports.commandCount = function(){
-    return Object.keys(commands).length;
-}
-
-bot.login(AuthDetails.email, AuthDetails.password);
